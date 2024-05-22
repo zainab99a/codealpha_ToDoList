bbf1ad850d64c735067c2af7a0fca601aab0d508
zainab99a <amanhal729@gmail.com>
2024-05-22 11:32:29 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById("input-box");
+const button = document.querySelector("button");
+const list = document.getElementById("list-container");
+
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("you must write something!");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        list.appendChild(li);
+        inputBox.value = '';
+        let span = document.createElement("span");
+        span.innerHTML = "x";
+        li.appendChild(span);
+    }
+    saveData();
+}
+list.addEventListener("click", (e)=>{
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+button.addEventListener("click", addTask);
+
+function saveData(){
+    localStorage.setItem("data", list.innerHTML);
+}
+function showTask(){
+    const savedData = localStorage.getItem("data");
+    if(savedData){
+        list.innerHTML = savedData;
+    }
+}
+window.addEventListener("load", showTask);
